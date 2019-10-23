ef7f358e7f503c80a20d2cc66c32181b0312fd34
ccyanxyz <790266922@qq.com>
2019-10-24 00:06:25 +0800

    call contract function ok

--- ethct.py
+++ ethct.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 from constants import *
 from ethct_contract import *
@@ -8,16 +9,20 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--save', help = "save compile output", action = 'store_true', default = False)
     parser.add_argument('-d', '--deploy', help = "deploy contract")
     parser.add_argument('-n', '--network', help = "choose network", default = "ropsten")
-    parser.add_argument('--call', help = "call contract function")
     parser.add_argument('--address', help = "contract address")
     parser.add_argument('--abi', help = "contract ABI file")
+    parser.add_argument('--call', help = "call contract function")
 
     args = parser.parse_args()
     print(args)
     if args.compile:
         contract = Contract(args.compile)
         contract.compile(save = args.save)
-    if args.deploy:
+    elif args.deploy:
         contract = Contract(args.deploy, provider_url = URL[args.network])
         contract.deploy()
-
+    elif args.call:
+        contract = Contract(abifile = args.abi, address = args.address, provider_url = URL[args.network])
+        arg_list = args.call.split(' ')
+        func_name = arg_list[0]
+        contract.call(func_name, arg_list[1:])
--- ethct_contract.py
+++ ethct_contract.py
@@ -7,16 +7,17 @@ from web3 import Web3, HTTPProvider
 class Contract:
 
     def __init__(self,
-            sourcefile,
+            sourcefile = None,
             abifile = None,
             bytecodefile = None,
             address = None,
             provider_url = None):
-        self.sourcefile = sourcefile
+        if sourcefile is not None:
+            self.sourcefile = sourcefile
         if provider_url is not None:
             self.provider_url = provider_url
         else:
-            self.provider_url = PROVIDER_URL
+            self.provider_url = URL['ropsten']
         self.web3 = Web3(HTTPProvider(self.provider_url))
         self.account = self.web3.eth.account.privateKeyToAccount(PRIVATE_KEY)
         self.web3.eth.defaultAccount = self.account
@@ -24,9 +25,11 @@ class Contract:
         if abifile is not None:
             self.abi = json.load(open(abifile))
         if bytecodefile is not None:
-            self.bytecode = json.load(open(bytecodefile))
+            self.bytecode = open(bytecodefile).read()
         if address is not None:
             self.address = address
+        if hasattr(self, 'abi') and hasattr(self, 'address'):
+            self.contract = self.web3.eth.contract(self.address, abi = self.abi)
 
     def compile(self, save = True):
         if hasattr(self, 'abi') and hasattr(self, 'bytecode'):
@@ -68,4 +71,32 @@ class Contract:
         self.address = receipt['contractAddress']
         print('contract address:', self.address)
         print('receipt:', receipt)
+
+    def call(self, func_name, arg_list):
+        abiinfo = {}
+        for info in self.abi:
+            if info['name'] == func_name:
+                abiinfo = info
+                break
+        func = self.contract.functions.__getitem__(func_name)
+        for i in range(len(abiinfo['inputs'])):
+            if abiinfo['inputs'][i]['type'] == 'bytes32':
+                arg_list[i] = str(arg_list[i]).encode()
+            if abiinfo['inputs'][i]['type'] == 'address':
+                arg_list[i] = self.web3.toChecksumAddress(arg_list[i])
+        if abiinfo['constant']:
+            result = func(*arg_list).call({'from': self.account.address}) 
+            if isinstance(result, bytes):
+                result = result.decode()
+            print(result)
+        else:
+            tx = func(*arg_list).buildTransaction({
+                'from': self.account.address,
+                'nonce': self.web3.eth.getTransactionCount(self.account.address),
+                'gasPrice': self.web3.toWei('21', 'gwei'),
+                })
+            signed = self.web3.eth.account.signTransaction(tx, self.account.privateKey)
+            txhash = self.web3.eth.sendRawTransaction(signed.rawTransaction)
+            receipt = self.web3.eth.waitForTransactionReceipt(txhash)
+            print(receipt)
 
