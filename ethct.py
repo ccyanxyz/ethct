87d496a5491e7bfbca8e4f61df6375d15d47da5e
ccyanxyz <790266922@qq.com>
2019-10-24 13:14:16 +0800

    add getstorage/getbalance/getblock

--- ethct.py
+++ ethct.py
@@ -19,8 +19,16 @@ if __name__ == "__main__":
     # tools
     # get tx by hash
     parser.add_argument('--gettx', help = "get transaction info by txhash")
+    # get block
+    parser.add_argument('--getblock', help = "get block by hash/block_number")
+    # get contract storage
+    parser.add_argument('--getstorage', help = "get contract storage")
+    parser.add_argument('--position', help = "contract storage position", type = int, default = 0)
+    # get account balance
+    parser.add_argument('--getbalance', help = "get account balance")
 
     args = parser.parse_args()
+    web3 = Web3(HTTPProvider(URL[args.network]))
     if args.compile:
         contract = Contract(args.compile)
         contract.compile(save = args.save)
@@ -33,6 +41,22 @@ if __name__ == "__main__":
         func_name = arg_list[0]
         contract.call(func_name, arg_list[1:])
     elif args.gettx:
-        web3 = Web3(HTTPProvider(URL[args.network]))
         txinfo = web3.eth.getTransaction(args.gettx)
         print(txinfo)
+    elif args.getblock:
+        block = None
+        fulltx = False
+        if args.getblock == 'latest':
+            block = web3.eth.getBlock('latest', full_transactions = fulltx)
+        elif len(args.getblock) > 15:
+            block = web3.eth.getBlock(args.getblock, full_transactions = fulltx)
+        elif len(args.getblock) <= 15:
+            block = web3.eth.getBlock(int(args.getblock), full_transactions = fulltx)
+        print(block)
+    elif args.getstorage:
+        result = web3.eth.getStorageAt(args.getstorage, int(args.position))
+        print(result.hex())
+    elif args.getbalance:
+        result = web3.eth.getBalance(web3.toChecksumAddress(args.getbalance))
+        balance = web3.fromWei(result, 'ether')
+        print(balance)
