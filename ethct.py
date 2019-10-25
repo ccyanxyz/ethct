cb8ea66a35f78e6d86c966e65489dfa4bda125cd
ccyanxyz <790266922@qq.com>
2019-10-25 19:47:00 +0800

    add config command

--- ethct.py
+++ ethct.py
@@ -4,6 +4,21 @@ from web3 import Web3, HTTPProvider
 from constants import *
 from ethct_contract import *
 
+default_config_path = os.environ['HOME'] + "/.ethct.json"
+
+def check_config():
+    with open(default_config_path, 'r') as f:
+        config = json.load(f)
+        if config['infurakey'] == '' and config['network'] != 'local':
+            print('please config your infura apikey')
+            exit()
+
+def check_privkey():
+    with open(default_config_path, 'r') as f:
+        config = json.load(f)
+        if config['privkey'] == '':
+            print('please config your private key')
+        
 def main():
     parser = argparse.ArgumentParser()
     # compile contract
@@ -27,16 +42,28 @@ def main():
     parser.add_argument('--position', help = "contract storage position", type = int, default = 0)
     # get account balance
     parser.add_argument('--getbalance', help = "get account balance")
+    # config
+    parser.add_argument('--config', help = "add config options", action = "store_true")
+    parser.add_argument('--privkey', help = "set private key")
+    parser.add_argument('--infurakey', help = "set infura apikey")
 
     args = parser.parse_args()
     web3 = Web3(HTTPProvider(URL[args.network]))
+    if not args.config:
+        if not os.path.exists(default_config_path):
+            print('use `ethct --config --privkey <PRIVKEY> --infurakey <APIKEY> --network <NETWORK>` to config the tool first')
+            exit()
+        check_config()
+
     if args.compile:
         contract = Contract(args.compile)
         contract.compile(save = args.save)
     elif args.deploy:
+        check_privkey()
         contract = Contract(args.deploy, provider_url = URL[args.network])
         contract.deploy(value = args.value)
     elif args.call:
+        check_privkey()
         contract = Contract(abifile = args.abi, address = args.address, provider_url = URL[args.network])
         arg_list = args.call.split(' ')
         func_name = arg_list[0]
@@ -61,6 +88,24 @@ def main():
         result = web3.eth.getBalance(web3.toChecksumAddress(args.getbalance))
         balance = web3.fromWei(result, 'ether')
         print(balance)
+    elif args.config:
+        config = {}
+        if not os.path.exists(default_config_path):
+            config = {
+                    'privkey': '',
+                    'network': 'ropsten',
+                    'infurakey': '',
+            }
+        else:
+            config = json.load(open(default_config_path, 'r'))
+        if args.network:
+            config['network'] = args.network
+        if args.privkey:
+            config['privkey'] = args.privkey
+        if args.infurakey:
+            config['infurakey'] = args.infurakey
+        f = open(default_config_path, 'w')
+        json.dump(config, f)
 
 if __name__ == '__main__':
     main()
