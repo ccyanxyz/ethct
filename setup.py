dfe14e7be6b3e07156bce60150215cfcc7d6902f
ccyanxyz <790266922@qq.com>
2019-10-31 10:39:59 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'ethct',
-    version = '19.10.31',
+    version = '19.10.31.2',
     description = 'Ethereum contract tool(command line)',
     url = 'https://github.com/ccyanxyz/ethct',
     author = 'ccyanxyz',
